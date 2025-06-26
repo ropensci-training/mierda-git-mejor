953658b71e0a725f1f26f3fdd39abbbd05a86e07
Maëlle Salmon <maelle.salmon@yahoo.se>
2025-06-26 14:46:04 +0200

    Initial commit

---
+++ example.R
@@ -0,0 +1,6 @@
+# nettoyage des donnés ----
+
+filter(starwars, hair_color == "none" & eye_color == "black" & eye_color == "black" & eye_color == "black")
+filter(starwars, hair_color == "none" | eye_color == "black")
+
+# modélisation ----
